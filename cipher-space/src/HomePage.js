43af01f083d82e1e90b80b664843eafd3dd5b9e0
Anais <anaiszed447@gmail.com>
2023-03-04 19:35:36 -0500

    Finished styling the content

--- cipher-space/src/HomePage.js
+++ cipher-space/src/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Page1 from './Page1';
 import { useNavigate } from 'react-router-dom';
-
+import "./HomePage.css"
 
 function HomePage(){
     const navigate = useNavigate();
@@ -17,13 +17,40 @@ function HomePage(){
     
 
     return(
+        <div className='home-page'>
         <>
-        <p> Home Page</p>
- 
-        <button onClick={navigateToAboutPage}>Go to Page 1</button>
-        <button onClick={navigateToSnagPage}>Go to Snag's page</button>
+        <div className='home-elements'>
+
+            <div className='title'>
+              <p>CIPHER SCAPE</p>
+            </div>
+
+        <div className='level-buttons'>
+    
+              <button>EASY</button>
+      
+
+       
+              <button >MEDIUM</button>
+      
+
+              <button >HARD</button>
+
+             
+        </div>
+        
+        <div>
+              <button className='start'onClick={navigateToAboutPage} >START</button>
+       </div>
+         
+       
+
+        </div>
+    
         </>
+        </div>
     )
 }
 
 export default HomePage;
+
